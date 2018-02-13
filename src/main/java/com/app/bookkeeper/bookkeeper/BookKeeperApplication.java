0430d94713a33d77991a9929e0b37cc3a1ae2786
houqisen <houqisen@gmail.com>
2018-02-12 19:48:18 -0800

    Initial commit

---
+++ src/main/java/com/app/bookkeeper/bookkeeper/BookKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.app.bookkeeper.bookkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKeeperApplication.class, args);
+	}
+}
